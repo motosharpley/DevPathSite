c637a3afc4511448352c5c1bdad6b3d46dec100a
Bryan Sharpley <motosharpley@gmail.com>
2022-08-28 17:47:42 -0700

    add queries, getRecent, getSimilar, getCategories

--- services/index.ts
+++ services/index.ts
@@ -3,7 +3,7 @@ const graphqlAPI = process.env.NEXT_PUBLIC_HYGRAPHCMS_ENDPOINT;
 
 export const getPosts = async () => {
   const query = gql`
-    query MyQuery {
+    query GetPosts {
       postsConnection {
         edges {
           node {
@@ -34,4 +34,63 @@ export const getPosts = async () => {
 
   const result = await request(graphqlAPI!, query);
   return result.postsConnection.edges;
+};
+
+export const getRecentPosts = async () => {
+  const query = gql`
+    query GetPostDetails() {
+      posts(
+        orderBy: createdAt_ASC
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+  const result = await request(graphqlAPI!, query);
+  return result.posts;
+};
+
+export const getSimilarPosts = async (categories: [], slug: string) => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]) {
+      posts(
+        where: {
+          slug_not: $slug
+          AND: { categories_some: { slug_in: $categories } }
+        }
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+  const result = await request(graphqlAPI!, query, { slug, categories });
+
+  return result.posts;
+};
+
+export const getCategories = async () => {
+  const query = gql`
+    query GetGategories {
+      categories {
+        name
+        slug
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI!, query);
+
+  return result.categories;
 };
