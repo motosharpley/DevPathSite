5aee2e203eca3f1c82c5f29a5e10c91d34f77b1d
Bryan Sharpley <motosharpley@gmail.com>
2022-08-30 09:32:14 -0700

    type check comments

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -4,7 +4,7 @@ import parse from 'html-react-parser';
 
 import { getComments } from '../services';
 
-const Comments = ({ slug }) => {
+const Comments = (slug: string) => {
   const [comments, setComments] = useState([]);
 
   useEffect(() => {
@@ -20,17 +20,22 @@ const Comments = ({ slug }) => {
           <h3 className="text-xl mb-8 font-semibold border-b pb-4">
             {comments.length} Comments
           </h3>
-          {comments.map((comment, index) => (
-            <div key={index} className="border-b border-gray-100 mb-4 pb-4">
-              <p className="mb-4">
-                <span className="font-semibold">{comment.name}</span> on{' '}
-                {moment(comment.createdAt).format('MMM DD, YYYY')}
-              </p>
-              <p className="whitespace-pre-line text-gray-600 w-full">
-                {parse(comment.comment)}
-              </p>
-            </div>
-          ))}
+          {comments.map(
+            (
+              comment: { name: string; createdAt: string; comment: string },
+              index
+            ) => (
+              <div key={index} className="border-b border-gray-100 mb-4 pb-4">
+                <p className="mb-4">
+                  <span className="font-semibold">{comment.name}</span> on{' '}
+                  {moment(comment.createdAt).format('MMM DD, YYYY')}
+                </p>
+                <p className="whitespace-pre-line text-gray-600 w-full">
+                  {parse(comment.comment)}
+                </p>
+              </div>
+            )
+          )}
         </div>
       )}
     </>
