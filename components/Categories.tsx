6814b551d7e6839987891f37e7ed4d0635019895
Bryan Sharpley <motosharpley@gmail.com>
2022-08-29 17:30:25 -0700

    add type checks

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -15,17 +15,21 @@ const Categories = () => {
   return (
     <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
       <h3 className="text-xl mb-8 font-semibold border-b pb-4">Categories</h3>
-      {categories.map((category, index) => (
-        <Link key={index} href={`/category/${category.slug}`}>
-          <span
-            className={`cursor-pointer block ${
-              index === categories.length - 1 ? 'border-b-0' : 'border-b'
-            } pb-3 mb-3`}
-          >
-            {category.name}
-          </span>
-        </Link>
-      ))}
+
+      {categories &&
+        categories.map(
+          (category: { name: string; slug: string }, index: number) => (
+            <Link key={index} href={`/category/${category.slug}`}>
+              <span
+                className={`cursor-pointer block ${
+                  index === categories.length - 1 ? 'border-b-0' : 'border-b'
+                } pb-3 mb-3`}
+              >
+                {category.name}
+              </span>
+            </Link>
+          )
+        )}
     </div>
   );
 };
