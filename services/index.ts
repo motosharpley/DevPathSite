71d81cfa2d2d39dba21b3397dd13f5a95bb76c27
Bryan Sharpley <motosharpley@gmail.com>
2022-08-29 22:24:05 -0700

    add get and submit comment queries

--- services/index.ts
+++ services/index.ts
@@ -128,3 +128,31 @@ export const getPostDetails = async (slug: string) => {
 
   return result.post;
 };
+
+export const getComments = async (slug: string) => {
+  const query = gql`
+    query GetComments($slug: String!) {
+      comments(where: { post: { slug: $slug } }) {
+        name
+        createdAt
+        comment
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI!, query, { slug });
+
+  return result.comments;
+};
+
+export const submitComment = async (obj) => {
+  const result = await fetch('/api/comments', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(obj),
+  });
+
+  return result.json();
+};
