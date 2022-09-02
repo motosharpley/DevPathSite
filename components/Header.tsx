ff2ea58f3796eb4b6f71164e102bf6cbb2ef7215
Bryan Sharpley <motosharpley@gmail.com>
2022-09-02 15:16:20 -0700

    add logo to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -21,9 +21,11 @@ const Header: React.FC = () => {
       <div className="border-b w-full inline-block  border-blue-400 py-8">
         <div className="md:float-left block">
           <Link href="/">
-            <span className="cursor-pointer font-bold text-4xl text-white ">
-              Dev Path
-            </span>
+            <img
+              src="./DevPathLogo-2121.png"
+              alt="DevPath Logo dev path your tech navigator"
+              width={140}
+            />
           </Link>
         </div>
         <div className="hidden md:float-left md:contents">
